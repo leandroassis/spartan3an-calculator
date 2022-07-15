75347e036e7939ad85a52391ad5415a923ba4c08
Leandro Assis dos Santos <assissantosleandro@poli.ufrj.br>
2022-07-15 16:58:40 -0300

    atualização do somador, nao funcional

--- modulo_somador_completo.vhd
+++ modulo_somador_completo.vhd
@@ -8,32 +8,16 @@
 
 ----------------------------------------------------------------------------------
 library IEEE;
+library work;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
+use work.BCD.all;
 
 entity modulo_somador_completo is
 	port(
-		-- Inicio dos digitos BCD de A
-		A0 : in unsigned(3 downto 0);
-		A1 : in unsigned(3 downto 0);
-		A2 : in unsigned(3 downto 0);
-		A3 : in unsigned(3 downto 0);
-		-- Fim dos dígitos BCD de A
-		
-		-- Inicio dos digitos BCD de B
-		B0 : in unsigned(3 downto 0);
-		B1 : in unsigned(3 downto 0);
-		B2 : in unsigned(3 downto 0);
-		B3 : in unsigned(3 downto 0);
-		-- Fim dos dígitos BCD de B
-		
-		-- Inicio dos digitos BCD da resposta
-		RESULTADO0 : out unsigned(3 downto 0);
-		RESULTADO1 : out unsigned(3 downto 0);
-		RESULTADO2 : out unsigned(3 downto 0);
-		RESULTADO3 : out unsigned(3 downto 0);
-		RESULTADO4 : out unsigned(3 downto 0)
-		-- FIM dos digitos BCD da resposta
+		A : in array_BCD_4DIGITOS;
+		B : in array_BCD_4DIGITOS;
+		RESULTADO : out array_BCD_5DIGITOS
 	);
 end modulo_somador_completo;
 
@@ -42,27 +26,27 @@ architecture Behavioral of modulo_somador_completo is
 	-- Chamada do modulo somador simples para fazer o somador em cascata
 	component modulo_somador_simples
 		port(
-			A  : in unsigned(3 downto 0);
-			B  : in unsigned(3 downto 0);
-			C_IN  : in unsigned;
-			C_OUT : out unsigned;
+			DIGITO_1  : in unsigned(3 downto 0);
+			DIGITO_2  : in unsigned(3 downto 0);
+			CARRY_IN  : in unsigned(3 downto 0);
+			CARRY_OUT : out unsigned(3 downto 0);
 			RESULT 	: out unsigned(3 downto 0)
 		);
 	end component;
 	
 	-- Variáveis auxiliares para guardar os carrys
-	signal aux0, aux1, aux2, aux3 : unsigned (3 downto 0) := "0000";
-	
+	signal aux : array_BCD_4DIGITOS := (('0', '0', '0', '0'), ('0', '0', '0', '0'), ('0', '0', '0', '0'), ('0', '0', '0', '0'));
+
 begin
 	
 	-- Soma do digito mais a direita em BCD
-	digito_0 : modulo_somador_simples port map(A0, B0, "0000", aux0, RESULTADO0);
+	digito_0 : modulo_somador_simples port map(A(0), B(0), "0000", aux(0), RESULTADO(0));
 	
-	digito_1 : modulo_somador_simples port map(A1, B1, aux0, aux1, RESULTADO1);
-	digito_2 : modulo_somador_simples port map(A2, B2, aux1, aux2, RESULTADO2);
-	digito_3 : modulo_somador_simples port map(A3, B3, aux2, aux3, RESULTADO3);
+	digito_1 : modulo_somador_simples port map(A(1), B(1), aux(0), aux(1), RESULTADO(1));
+	digito_2 : modulo_somador_simples port map(A(2), B(2), aux(1), aux(2), RESULTADO(2));
+	digito_3 : modulo_somador_simples port map(A(3), B(3), aux(2), aux(3), RESULTADO(3));
 	
 	-- Atribuição do digito BCD mais a esquerda
-	RESULTADO4 <= aux3;
+	RESULTADO(4) <= aux(3);
 end Behavioral;
 
