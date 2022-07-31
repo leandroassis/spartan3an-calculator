492746d11e3f789a04d87a388edee2b477ed9b66
Leandro Assis <assissantosleandro@poli.ufrj.br>
2022-07-31 20:51:18 -0300

    quase 100%

--- modulo_principal.vhd
+++ modulo_principal.vhd
@@ -55,13 +55,31 @@ architecture Behavioral of modulo_principal is
 		);	
 	end component;
 	
+	component modulo_somador_completo
+		port(
+			A : in array_BCD_4DIGITOS;
+			B : in array_BCD_4DIGITOS;
+			RESULTADO : out array_BCD_5DIGITOS
+		);
+	end component;
+	
+	component modulo_multiplicador_completo
+		port(
+			A : in array_BCD_4DIGITOS;
+			B : in array_BCD_4DIGITOS;
+			RESULT : out array_BCD_8DIGITOS
+		);
+	end component;
+	
 	type estados_maquina is (digito0_0, digito1_0, digito2_0, digito3_0,
 									 digito0_1, digito1_1, digito2_1, digito3_1,
 									 resultado, operacao, igual);
 	signal estado_atual, proximo_estado : estados_maquina := digito0_0;
-	signal numero_code : std_logic_vector(3 downto 0);
+	signal numero_code, op : std_logic_vector(3 downto 0);
 	signal A,B : array_BCD_4DIGITOS;
-	signal write_lcd, next_step, is_empty : std_logic;
+	signal soma : array_BCD_5DIGITOS;
+	signal multiplicacao : array_BCD_8DIGITOS;
+	signal write_lcd, is_empty : std_logic;
 	
 begin
 	
@@ -69,22 +87,21 @@ begin
 	
 	kbd: kb_code port map(clk, reset_maq, ps2d=>ps2d, ps2c=>ps2c, not is_empty, numero_code, is_empty);
 	
+	soma : modulo_somador_completo port map(A, B, soma);
+	
+	multiplicador : modulo_multiplicador_completo port map(A, B, multiplicacao);
+	
 	-- FSM para ler digitos
 	
 	process(numero_code)
 	begin
 		if numero_code(3 downto 0) = X"1101" then
 			estado_atual <= proximo_estado;
+			write_lcd <= '1';
+		else 
+			code_escrita <= numero_code(3 downto 0);
+			write_lcd <= '0';
 		end if;
-	end process;
-	
-	process(clk)
-		begin
-			if clk'event and clk = '1' then
-				write_lcd <= send_to_lcd;
-			elsif clk'event and clk='0' then
-				write_lcd <= '0';
-			end if;
 	end process;
 	
 	process(numero_code, reset_maq)
@@ -95,66 +112,66 @@ begin
 				case estado_atual is
 					when digito0_0 =>
 						if numero_code /= X"0D" then
-							A(0) <= numero_code;
+							A(0) <= numero_code(3 downto 0);
 							proximo_estado <= digito0_1;
 						else 
 							proximo_estado <= digito0_0;
 						end if;
 					when digito0_1 =>
 						if numero_code /= X"0D" then
-							A(0) <= numero_code;
-							proximo_estado <= digito0_1;
+							A(1) <= numero_code(3 downto 0);
+							proximo_estado <= digito0_2;
 						else 
-							proximo_estado <= digito0_0;
+							proximo_estado <= digito0_1;
 						end if;
 					when digito0_2 =>
 						if numero_code /= X"0D" then
-							A(0) <= numero_code;
-							proximo_estado <= digito0_1;
+							A(2) <= numero_code(3 downto 0);
+							proximo_estado <= digito0_3;
 						else 
-							proximo_estado <= digito0_0;
+							proximo_estado <= digito0_2;
 						end if;
 					when digito0_3 =>
 						if numero_code /= X"0D" then
-							A(0) <= numero_code;
-							proximo_estado <= digito0_1;
+							A(3) <= numero_code(3 downto 0);
+							proximo_estado <= operacap;
 						else 
-							proximo_estado <= digito0_0;
+							proximo_estado <= digito0_3;
 						end if;
 					when operacao =>
-						if numero_code /= X"0D" then
-							A(0) <= numero_code;
-							proximo_estado <= digito0_1;
+						if OPCODE = "0001" or OPCODE = "1000" then
+							op <= OPCODE;
+							proximo_estado <= digito1_0;
 						else 
-							proximo_estado <= digito0_0;
+							proximo_estado <= operacao;
 						end if;
 					when digito1_0 =>
 						if numero_code /= X"0D" then
-							A(0) <= numero_code;
-							proximo_estado <= digito0_1;
+							B(0) <= numero_code(3 downto 0);
+							proximo_estado <= digito1_1;
 						else 
-							proximo_estado <= digito0_0;
+							proximo_estado <= digito1_0;
 						end if;
 					when digito1_1 =>
 						if numero_code /= X"0D" then
-							A(0) <= numero_code;
-							proximo_estado <= digito0_1;
+							B(1) <= numero_code(3 downto 0);
+							proximo_estado <= digito1_2;
 						else 
-							proximo_estado <= digito0_0;
+							proximo_estado <= digito1_1;
 						end if;
 					when digito1_2 =>
 						if numero_code /= X"0D" then
-							A(0) <= numero_code;
-							proximo_estado <= digito0_1;
+							B(2) <= numero_code(3 downto 0);
+							proximo_estado <= digito1_3;
 						else 
-							proximo_estado <= digito0_0;
+							proximo_estado <= digito1_2;
 						end if;
 					when digito1_3 =>
 						if numero_code /= X"0D" then
-							A(0) <= numero_code;
-							proximo_estado <= digito0_1;
+							B(3) <= numero_code(3 downto 0);
+							proximo_estado <= resultado;
 						else 
-							proximo_estado <= digito0_0;
+							proximo_estado <= digito1_3;
 						end if;
 					when resultado =>
 						proximo_estado <= resultado;
@@ -163,6 +180,18 @@ begin
 			end if;
 	end process;
 	
+	-- FSM para apresentar o resultado
+	
+	process(clk)
+	begin
+		if clk'event and clk='1' then
+			if op = "0001" then -- soma
+				code_escrita <= soma(4);
+			elsif op = "1000" then -- multipli
+				code_escrita <= multiplicacao(7);
+			end if;
+		end if;
+	end process;
 
 end Behavioral;
 
